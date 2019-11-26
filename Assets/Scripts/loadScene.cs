15921e977d9dfbf5972143d5550f9ed150f76ba5
unknown <c00220950@itcarlow.ie>
2019-11-26 16:57:50 +0000

    Added level select buttons

---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour
+{
+    public void LoadAScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
