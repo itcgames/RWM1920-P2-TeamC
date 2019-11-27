2993f50c9d90074afadb13a221453efe76b3fd02
unknown <c00220950@itcarlow.ie>
2019-11-27 11:29:43 +0000

    Implemented pause menu to the game screen

---
+++ Assets/Scripts/PauseHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour
+{
+    public GameObject puasePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            switchPausePanel();
+        }
+    }
+
+    public void switchPausePanel()
+    {
+        puasePanel.SetActive(!puasePanel.activeSelf);
+    }
+}
