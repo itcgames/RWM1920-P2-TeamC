be0fff95a1713fccc44d2bde9d56b92f8e624fb2
unknown <c00220950@itcarlow.ie>
2019-11-28 12:58:59 +0000

    Removing debug messages

--- Assets/Scripts/ComponentPanel.cs
+++ Assets/Scripts/ComponentPanel.cs
@@ -14,22 +14,18 @@ public class ComponentPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     {
         m_placeObject = Instantiate(m_obj, this.transform.position, Quaternion.identity);
         m_followPointer = true;
-        //Output the name of the GameObject that is being clicked
-        Debug.Log(name + "Game Object Click in Progress");
     }
 
     //Detect if clicks are no longer registering
     public void OnPointerUp(PointerEventData pointerEventData)
     {
         m_followPointer = false;
-        Debug.Log(name + "No longer being clicked");
     }
 
     void Update()
     {
         if(m_followPointer && m_placeObject != null)
         {
-            Debug.Log(Input.mousePosition);
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             newPosition.z = 1;
             m_placeObject.transform.position = newPosition;
