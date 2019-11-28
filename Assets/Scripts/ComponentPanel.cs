0a7eac86bd7e45d9fcd9a3c02b11de4a0e83ac96
unknown <c00220950@itcarlow.ie>
2019-11-28 12:08:49 +0000

    Implemented component panel

---
+++ Assets/Scripts/ComponentPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ComponentPanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject m_obj;
+    private bool m_followPointer;
+    private GameObject m_placeObject = null;
+
+    //Detect current clicks on the GameObject (the one with the script attached)
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        m_placeObject = Instantiate(m_obj, this.transform.position, Quaternion.identity);
+        m_followPointer = true;
+        //Output the name of the GameObject that is being clicked
+        Debug.Log(name + "Game Object Click in Progress");
+    }
+
+    //Detect if clicks are no longer registering
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        m_followPointer = false;
+        Debug.Log(name + "No longer being clicked");
+    }
+
+    void Update()
+    {
+        if(m_followPointer && m_placeObject != null)
+        {
+            Debug.Log(Input.mousePosition);
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition.z = 1;
+            m_placeObject.transform.position = newPosition;
+        }
+    }
+}
