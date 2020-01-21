c640c7663c5a3e096be699fdd5bffeb012e7b4eb
unknown <c00220950@itcarlow.ie>
2020-01-21 12:40:22 +0000

    updated cannon prefab

--- Assets/Scripts/StartPointScript.cs
+++ Assets/Scripts/StartPointScript.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class StartPointScript : MonoBehaviour
 {
-    private GameObject m_player;
     private SpriteRenderer m_sprite;
     void Start()
     {
         m_sprite = GetComponent<SpriteRenderer>();
-        m_player = Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
-        m_player.transform.position = new Vector3(transform.position.x, transform.position.y - m_sprite.bounds.size.y);
+        GameObject player = Instantiate(Resources.Load<GameObject>("Prefabs/Player"));
+        player.transform.position = new Vector3(transform.position.x, transform.position.y - m_sprite.bounds.size.y);
     }
 
     public void Reset()
     {
         GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(transform.position.x, transform.position.y - m_sprite.bounds.size.y);
-        m_player.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity = Vector3.zero;
     }
 }
